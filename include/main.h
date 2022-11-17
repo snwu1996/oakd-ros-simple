5b739fa898c98b0f7a939113765d1b3887606485
Shu-Nong Wu <shunongwu@gmail.com>
2022-11-16 18:55:01 -0800

    updated depthai-core version to 2.19.0 in order to get the OAK-D W POE working.

--- include/main.h
+++ include/main.h
@@ -244,7 +244,8 @@ void oakd_ros_class::main_initialize(){
     std::shared_ptr<dai::node::XLinkOut> xoutIMU = pipeline.create<dai::node::XLinkOut>();
     xoutIMU->setStreamName("imu");
 
-    IMU_node->enableIMUSensor({dai::IMUSensor::ACCELEROMETER_RAW, dai::IMUSensor::GYROSCOPE_RAW, dai::IMUSensor::ROTATION_VECTOR}, fps_IMU);
+    // IMU_node->enableIMUSensor({dai::IMUSensor::ACCELEROMETER_RAW, dai::IMUSensor::GYROSCOPE_RAW, dai::IMUSensor::ROTATION_VECTOR}, fps_IMU);
+    IMU_node->enableIMUSensor({dai::IMUSensor::ACCELEROMETER_RAW, dai::IMUSensor::GYROSCOPE_RAW}, fps_IMU);
     IMU_node->setBatchReportThreshold(1);
     IMU_node->setMaxBatchReports(28);
     IMU_node->out.link(xoutIMU->input);
